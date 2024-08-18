a9670a3afc8db1f73a3d9ecbcad2723d1792485c
officialanne <144495906+officialanne@users.noreply.github.com>
2024-08-18 23:25:15 +0100

    Update firebase.js with analytics
    adding google analytics

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
 
 
 // Your web app's Firebase configuration
@@ -16,4 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const analytics = getAnalytics(app);
 export default db;
+
